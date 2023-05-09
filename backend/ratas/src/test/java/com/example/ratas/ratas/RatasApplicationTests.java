ecafe4451b40677081c9be97b078f25e9b04156e
Francisca Palma <f.p.chii@gmail.com>
2023-05-09 16:07:23 -0400

    first commit

---
+++ backend/ratas/src/test/java/com/example/ratas/ratas/RatasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ratas.ratas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

caac610fd669c21715492bcfdffe40f3e29b0fda
Francisca Palma <f.p.chii@gmail.com>
2023-05-09 19:16:15 -0400

    configraciones antes de

--- backend/ratas/src/main/java/com/example/ratas/ratas/RatasApplication.java
+++ backend/ratas/src/main/java/com/example/ratas/ratas/RatasApplication.java
@@ -2,8 +2,6 @@ package com.example.ratas.ratas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 public class RatasApplication {
@@ -12,8 +10,4 @@ public class RatasApplication {
 		SpringApplication.run(RatasApplication.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
-	}
 }

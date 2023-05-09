42d0c83af003f0714c80d0c2b00ff56b4809a87e
Francisca Palma <f.p.chii@gmail.com>
2023-05-09 19:38:45 -0400

    borrado anotacion repository

--- backend/ratas/src/main/java/com/example/ratas/ratas/repository/PointTypeRepository.java
+++ backend/ratas/src/main/java/com/example/ratas/ratas/repository/PointTypeRepository.java
@@ -1,11 +1,9 @@
 package com.example.ratas.ratas.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.example.ratas.ratas.model.PointType;
 
-@Repository
 public interface PointTypeRepository extends JpaRepository<PointType, Long> {
 
 }
